7d0319af155691d2419c7d88dcfa60ab535a73a9
nsmajm <md.nasimanjum@MdNasims-MacBook-Pro.local>
2024-03-01 22:41:57 +0000

    update image in hero

--- src/components/posts/HeroPost.vue
+++ src/components/posts/HeroPost.vue
@@ -1,17 +1,36 @@
 <script setup lang="ts">
 import axios from 'axios'
 import { useQuery } from '@tanstack/vue-query'
+import { ref, watchEffect } from 'vue'
 const getPost = async () => await axios.get(`http://localhost:8000/api/featured`)
 
 const { data } = useQuery({
   queryKey: ['post'],
   queryFn: getPost
 })
+const isMobile = ref(window.innerWidth < 768)
+
+watchEffect(() => {
+  // Update isMobile when window is resized
+  const handleResize = () => {
+    isMobile.value = window.innerWidth < 768
+  }
+
+  // Add event listener on component mount
+  window.addEventListener('resize', handleResize)
+
+  // Remove event listener on component unmount to avoid memory leaks
+  return () => {
+    window.removeEventListener('resize', handleResize)
+  }
+})
 </script>
 <template lang="">
   <div
-    :style="{ backgroundImage: `url('${data?.data?.large_image}')` }"
-    class="w-screen bg-[length:100%] scale-[200px] object-fill bg-bottom bg-no-repeat  from-[rgba(0,0,0,0.31)] to-[rgba(0,0,0,0.31)] text-white"
+    :style="{
+      backgroundImage: `url('${isMobile ? data?.data?.image_url : data?.data?.large_image}')`
+    }"
+    class="w-screen bg-[length:100%] scale-[200px] object-fill bg-bottom bg-no-repeat from-[rgba(0,0,0,0.31)] to-[rgba(0,0,0,0.31)] text-white"
   >
     <div class="container mx-auto md:pt-[400px] md:pb-[80px] pt-[87px] pb-[25px] p-5">
       <div class="md:max-w-[600px] space-y-3">
