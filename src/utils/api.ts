5e0656ed6bf2da1a07ce205c9eb5f656ee959c7e
nsmajm <md.nasimanjum@MdNasims-MacBook-Pro.local>
2024-03-02 03:45:55 +0000

    update

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const http = axios.create({
-  baseURL: `https://vue-blog-backend-git-main-nsmajm.vercel.app/api/`,
+  baseURL: `https://vue-blog-backend-git-main-nsmajm.vercel.app/api/api/`,
 })
